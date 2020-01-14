936d19c89c83d032c49e91698002f4a0f3bc3755
yakov0401 <59759922+yakov0401@users.noreply.github.com>
2020-01-14 13:11:06 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute ('src','images/firefox2.png');
+    if(mySrc === 'firefox-icon.png') {
+      myImage.setAttribute ('src','firefox2.png');
     } else {
-      myImage.setAttribute ('src','images/firefox-icon.png');
+      myImage.setAttribute ('src','firefox-icon.png');
     }
 }
 var myButton = document.querySelector('button');
